434c2551d052da8e456e9ead139a139c6e359115
Brett Gadd <brettgadd@gmail.com>
2022-05-30 12:01:58 -0700

    messing around

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/index.css'
-import type { AppProps } from 'next/app'
+import 'styles/global.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
-
-export default MyApp
